26d3c68b3bc6184f6a21731b5e004caf469f89e3
karshubx <shubhamx.kar@intel.com>
2020-07-17 01:22:25 +0530

    classifier_startup.py: Added multiple UDFs support in Kapacitor
    This MR include the new sample directory where all the samples
    of running multiple UDFs including the README are kept.
    
    Changes:
    1. Modify the classifier_startup.py to run multiple tasks and
       multiple UDFs based on etcd config
    2. Refactoring of the code due to pylint and pycodestyle
    3. Modified the structure of config.json to include multiple tasks
       and multiple UDFs support
    
    JIRA Ticket ID: IAPOC9-3873
    
    Signed-off-by: karshubx <shubhamx.kar@intel.com>

---
+++ udfs/humidity_classifier.py
@@ -0,0 +1,121 @@
+# Copyright (c) 2020 Intel Corporation.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to
+# deal in the Software without restriction, including without limitation the
+# rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+# sell copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM,OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+# IN THE SOFTWARE.
+
+
+""" Custom UDF to filter the humidity data
+    and send back to Kapacitor
+"""
+import os
+import sys
+import json
+from kapacitor.udf.agent import Agent, Handler, Server
+from kapacitor.udf import udf_pb2
+import signal
+import stat
+import logging
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s %(levelname)s:%(name)s: %(message)s')
+logger = logging.getLogger()
+
+
+# Filter humid points it receives back to Kapacitor
+class HumidityClassifierHandler(Handler):
+    def __init__(self, agent):
+        self._agent = agent
+
+    def info(self):
+        """ Return the InfoResponse. Describing the properties of this Handler
+        """
+        response = udf_pb2.Response()
+        response.info.wants = udf_pb2.STREAM
+        response.info.provides = udf_pb2.STREAM
+        return response
+
+    def init(self, init_req):
+        """ Initialize the Handler with the provided options.
+        """
+        response = udf_pb2.Response()
+        response.init.success = True
+        return response
+
+    def snapshot(self):
+        """ Create a snapshot of the running state of the process.
+        """
+        response = udf_pb2.Response()
+        response.snapshot.snapshot = b''
+        return response
+
+    def restore(self, restore_req):
+        """ Restore a previous snapshot.
+        """
+        response = udf_pb2.Response()
+        response.restore.success = False
+        response.restore.error = 'not implemented'
+        return response
+
+    def begin_batch(self, begin_req):
+        """ A batch has begun.
+        """
+        raise Exception("not supported")
+
+    def point(self, point):
+        """ A point has arrived.
+        """
+        point_dict = point.fieldsDouble
+        # Taking the humidity data in point
+        # Writing back to Kapacitor if humidity
+        # value if greater than 25
+        humid = point_dict['humidity']
+        if humid > 25:
+            response = udf_pb2.Response()
+            response.point.CopyFrom(point)
+            self._agent.write_response(response, True)
+
+    def end_batch(self, end_req):
+        """ The batch is complete.
+        """
+        raise Exception("not supported")
+
+
+class Accepter(object):
+    _count = 0
+
+    def accept(self, conn, addr):
+        """ Create a new agent/handler for each new connection.
+            Count and log each new connection and termination.
+        """
+        self._count += 1
+        a = Agent(conn, conn)
+        h = HumidityClassifierHandler(a)
+        a.handler = h
+
+        logger.info("Starting Agent for connection %d", self._count)
+        a.start()
+        a.wait()
+        logger.info("Agent finished connection %d", self._count)
+
+
+if __name__ == '__main__':
+    path = "/tmp/humidity_classifier"
+    server = Server(path, Accepter())
+    os.chmod(path, stat.S_IRWXU | stat.S_IRGRP | stat.S_IXGRP |
+             stat.S_IROTH | stat.S_IXOTH)
+    logger.info("Started server")
+    server.serve()
